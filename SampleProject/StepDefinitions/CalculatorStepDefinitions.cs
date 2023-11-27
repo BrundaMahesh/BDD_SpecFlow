bae8e7a3b41f56d34d98736ec8e48835c18d4f04
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-27 11:01:45 +0530

    added new method

--- SampleProject/StepDefinitions/CalculatorStepDefinitions.cs
+++ SampleProject/StepDefinitions/CalculatorStepDefinitions.cs
@@ -1,44 +1,48 @@
+using NUnit.Framework;
+
 namespace SampleProject.StepDefinitions
 {
     [Binding]
     public sealed class CalculatorStepDefinitions
     {
         // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+       private int firstNumber,secondNumber,sum,difference;
 
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            throw new PendingStepException();
+            this.firstNumber = number;
         }
 
         [Given("the second number is (.*)")]
         public void GivenTheSecondNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-
-            throw new PendingStepException();
+            this.secondNumber = number;
         }
 
         [When("the two numbers are added")]
         public void WhenTheTwoNumbersAreAdded()
         {
-            //TODO: implement act (action) logic
-
-            throw new PendingStepException();
+            this.sum= this.firstNumber+this.secondNumber;
         }
 
-        [Then("the result should be (.*)")]
+        [Then("the sum should be (.*)")]
         public void ThenTheResultShouldBe(int result)
         {
-            //TODO: implement assert (verification) logic
-
-            throw new PendingStepException();
+            Assert.AreEqual(result, this.sum);
         }
+
+        [When(@"the second number is subtracted from the first number")]
+        public void WhenTheSecondNumberIsSubtractedFromTheFirstNumber()
+        {
+            this.difference = this.firstNumber - this.secondNumber;  
+        }
+
+        [Then(@"the difference should be (.*)")]
+        public void ThenTheDifferenceShouldBe(int result)
+        {
+            Assert.AreEqual(result, this.difference);
+        }
+
     }
 }
