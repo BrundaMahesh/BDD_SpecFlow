b8c6dd33e2f1bbd39a59be34c6397ce5028667f8
BrundaMahesh <brunda.mahesh@ust.com>
2023-12-01 11:09:37 +0530

    commit

--- LinkedInTests/StepDefinitions/LoginSteps.cs
+++ LinkedInTests/StepDefinitions/LoginSteps.cs
@@ -1,5 +1,7 @@
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using TechTalk.SpecFlow;
 
@@ -10,44 +12,63 @@ namespace LinkedInTests.StepDefinitions
     {
         public IWebDriver? driver;
 
-        [BeforeScenario]
-        public void Setup()
-        {
-            driver = new ChromeDriver();
-        }
-        [AfterScenario]
-        public void Cleanup()
-        {
-            driver?.Quit();
-        }
+        //[BeforeScenario]
+        //public void Setup()
+        //{
+        //    driver = new ChromeDriver();
+        //}
+        //[AfterScenario]
+        //public void Cleanup()
+        //{
+        //    driver?.Quit();
+        //}
         [Given(@"User will be on the login page")]
         public void GivenUserWillBeOnTheLoginPage()
         {
-            throw new PendingStepException();
+            driver = new ChromeDriver();
+            driver.Url = "https://www.linkedin.com";
         }
 
         [When(@"User will enter username")]
         public void WhenUserWillEnterUsername()
         {
-            throw new PendingStepException();
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(10);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(100);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fluentWait.Message = "Element not found";
+
+            IWebElement emailInput = fluentWait.Until(d => d.FindElement(By.Id("session_key")));
+            emailInput.SendKeys("abc@gmail.com");
         }
 
         [When(@"User will enter password")]
         public void WhenUserWillEnterPassword()
         {
-            throw new PendingStepException();
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(10);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(100);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fluentWait.Message = "Element not found";
+
+            IWebElement passwordInput = fluentWait.Until(d => d.FindElement(By.Id("session_password")));
+            passwordInput.SendKeys("12345");
         }
 
         [When(@"User will click on login button")]
         public void WhenUserWillClickOnLoginButton()
         {
-            throw new PendingStepException();
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].scrollIntoView(true);", driver.FindElement(By.XPath("//button[@type='submit']")));
+
+            Thread.Sleep(5000);
+            js.ExecuteScript("arguments[0].click();", driver.FindElement(By.XPath("//button[@type='submit']")));
         }
 
         [Then(@"User will be redirected to home page")]
         public void ThenUserWillBeRedirectedToHomePage()
         {
-            throw new PendingStepException();
+            Assert.That(driver.Title.Contains("Log In"));
         }
     }
 }
