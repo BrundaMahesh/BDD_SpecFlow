43bd3d7cf4411cb0798b40d506f5a7a27eda9f40
BrundaMahesh <brunda.mahesh@ust.com>
2023-12-05 18:05:38 +0530

    last commit

--- BunnyCart/StepDefinitions/SearchAndAddToCartSteps.cs
+++ BunnyCart/StepDefinitions/SearchAndAddToCartSteps.cs
@@ -11,37 +11,38 @@ namespace BunnyCart.StepDefinitions
     [Binding]
     public class SearchAndAddToCartSteps:CoreCodes
     {
+
         IWebDriver? driver = AllHooks.driver;
         string? label;
 
-        [Given(@"Search page is loaded")]
-        public void GivenSearchPageIsLoaded()
-        {
-            driver.Url = "https://www.bunnycart.com/catalogsearch/result/?q=water";
-        }
+        //[Given(@"Search page is loaded")]
+        //public void GivenSearchPageIsLoaded()
+        //{
+        //    driver.Url = "https://www.bunnycart.com/catalogsearch/result/?q=water";
+        //}
 
-        [When(@"User selects a '([^']*)'")]
-        public void WhenUserSelectsA(string prodno)
-        {
-            IWebElement prod=driver.FindElement(By.XPath("//*[@id=\"amasty-shopby-product-list\"]/div[2]/ol/li[1]/div/div[2]/strong/a[" +prodno + "]"));
-            label = prod.Text;
-            prod.Click();
-        }
+        //[When(@"User selects a '([^']*)'")]
+        //public void WhenUserSelectsA(string prodno)
+        //{
+        //    IWebElement prod=driver.FindElement(By.XPath("//*[@id=\"amasty-shopby-product-list\"]/div[2]/ol/li[1]/div/div[2]/strong/a[" +prodno + "]"));
+        //    label = prod.Text;
+        //    prod.Click();
+        //}
 
-        [Then(@"Product page is loaded")]
-        public void ThenProductPageIsLoaded()
-        {
-            TakeScreenShot(driver);
-            try
-            {
-                Assert.That(driver.FindElement(By.XPath("//h1[@class='page-title']")).Text.Equals(label));
-                LogTestResult("Product Page Test", "Product Page Test success");
-            }
-            catch (AssertionException ex)
-            {
-                LogTestResult("Product Page Test", "Product Page Test Failed", ex.Message);
-            }
-        }
+        //[Then(@"Product page is loaded")]
+        //public void ThenProductPageIsLoaded()
+        //{
+        //    TakeScreenShot(driver);
+        //    try
+        //    {
+        //        Assert.That(driver.FindElement(By.XPath("//h1[@class='page-title']")).Text.Equals(label));
+        //        LogTestResult("Product Page Test", "Product Page Test success");
+        //    }
+        //    catch (AssertionException ex)
+        //    {
+        //        LogTestResult("Product Page Test", "Product Page Test Failed", ex.Message);
+        //    }
+        //}
 
 
 
--- BunnyCart/StepDefinitions/SearchSteps.cs
+++ BunnyCart/StepDefinitions/SearchSteps.cs
@@ -5,6 +5,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using Serilog;
 using System;
+using System.Reflection.Emit;
 using TechTalk.SpecFlow;
 
 namespace BunnyCart.StepDefinitions
@@ -78,5 +79,29 @@ namespace BunnyCart.StepDefinitions
                 LogTestResult("Title Test", "Title Test Failed", ex.Message);
             }
         }
+        string label;
+        [When(@"User selects a '([^']*)'")]
+        public void WhenUserSelectsA(string prodno)
+        {
+            IWebElement prod = driver.FindElement(By.XPath("//*[@id=\"amasty-shopby-product-list\"]/div[2]/ol/li[1]/div/div[2]/strong/a[" + prodno + "]"));
+            label = prod.Text;
+            prod.Click();
+        }
+
+        [Then(@"Product page is loaded")]
+        public void ThenProductPageIsLoaded()
+        {
+            TakeScreenShot(driver);
+            try
+            {
+                Assert.That(driver.FindElement(By.XPath("//h1[@class='page-title']")).Text.Equals(label));
+                LogTestResult("Product Page Test", "Product Page Test success");
+            }
+            catch (AssertionException ex)
+            {
+                LogTestResult("Product Page Test", "Product Page Test Failed", ex.Message);
+            }
+        }
+
     }
 }
